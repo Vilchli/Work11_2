ff6d042861f387ff0a756e13a254fa2e06a14ade
Vil <Vil.king.li@mail.ru>
2023-06-08 19:46:55 +0300

    string_title and string_upper

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def string_upper(string):
     return string.upper()
+
+
+def string_title(string):
+    """функция, которая делает заглавными первые буквы каждого слова в строке"""
+    return string.title()
